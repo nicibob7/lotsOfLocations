8fbeb461f637605409f2b50701c6157d3924d08e
Nicolas Sanschagrin <nsanschagrin7@gmail.com>
2023-07-31 09:50:15 +0100

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+const port = 3000;
 
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use(express.static('frontend'));
+
+app.get('/', (req, res) => {
+    res.send()
+})
+
+app.listen(port, () => {
+    console.log(`API listening on port ${port}.`);
+})
