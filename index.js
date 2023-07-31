70ca2cf36a40566a0b532fcd714d6fa734fe0868
Nicolas Sanschagrin <nsanschagrin7@gmail.com>
2023-07-31 15:16:22 +0100

    syntax

--- index.js
+++ index.js
@@ -7,12 +7,14 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use(express.static('frontend'));
+// app.use(express.static('frontend'));
 app.use(express.static('frontend/homepage'));
-app.use(express.static('/frontend/art/art.html'));
-app.use(express.static('/frontend/history/history.html'));
-app.use(express.static('/frontend/music/music.html'));
-app.use(express.static('/frontend/language/language.html'));
+app.use('/art', express.static('frontend/art'));
+
+
+// app.use(express.static('/frontend/history/history.html'));
+// app.use(express.static('/frontend/music/music.html'));
+// app.use(express.static('/frontend/language/language.html'));
 
 app.get('/', (req, res) => {
     res.send('frontend/index.html')
