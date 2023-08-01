c92c4f8a553ebc753b290f75967c52af80f4a56d
Franki Callard <francescalily@outlook.com>
2023-08-01 19:48:21 +0100

    made hints to come up second time they answer wrong

--- frontend/map/index.js
+++ frontend/map/index.js
@@ -7,9 +7,6 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map); 
 
 
-
-
-
 const popUpContent = document.querySelector('.popup-content')
 const question = document.querySelector('.popup-content p')
 const userInput = document.querySelector('.popup-content')
@@ -21,78 +18,82 @@ const howToPlay = document.querySelector('#container')
 let isShowingRules = false;
 
 
-
-
-
 const questions = [
     {
       question: "What country is this?",
       correctAnswer: "uruguay",
       nextQuestion: "What is the capital city of this country?",
+      hint: "Hint: It's a country in South America",
     },
     {
       question: "What is the capital city of this country?",
       correctAnswer: "montevideo",
       nextQuestion: "What language is spoken most in this country?",
+      hint: "Hint: It's a ten letter word, the last five letters spell video",
     },
     {
       question: "What language is spoken most in this country?",
       correctAnswer: "spanish",
       nextQuestion: "Press the button here to learn about Uruguay!",
+      hint: "Hint: This is the word spelt backwards - hsinaps",
     },
   ];
 
+
+
   let currentQuestionIndex = 0;
+  let consecutiveWrongAnswers = 0;
+
+
 
   function moveToNextQuestion() {
     currentQuestionIndex++;
-    
-    if (currentQuestionIndex < questions.length) {
+    if (currentQuestionIndex < questions.length ) {
         question.textContent = questions[currentQuestionIndex].question;
         answer.value = '';
-    } else {
-        alert('All the questions have been answered,now time to learn about Uruguay!');
-        question.textContent = questions[currentQuestionIndex - 1].nextQuestion;
-        submitAnswer.style.fontSize = "50px";
-        submitAnswer.textContent = 'PRESS HERE';
-        const labelElement = document.querySelector("label[for='answer']");
-        labelElement.remove();
-        answer.remove();
-        submitAnswer.addEventListener("click", function () {
-            window.location.href = "https://www.bbc.com";
-        });
+        } 
     }
-  }
+  
+
 
   submitAnswer.addEventListener('click', function (e) {
     e.preventDefault();
     const answerInput = answer.value.toLowerCase();
-    
     if (answerInput === questions[currentQuestionIndex].correctAnswer) {
         console.log(answerInput);
         answer.value = '';
-        alert("Correct! Now try the next one");
+
+        if (currentQuestionIndex < questions.length - 1) {
+            alert('Correct! Now try the next one');
+        } else {
+            alert('All the questions have been answered, now time to learn about Uruguay!');
+      question.textContent = questions[currentQuestionIndex - 1].nextQuestion;
+      submitAnswer.style.fontSize = '50px';
+      submitAnswer.textContent = 'PRESS HERE';
+      const labelElement = document.querySelector("label[for='answer']");
+      labelElement.remove();
+      answer.remove();
+      submitAnswer.addEventListener('click', function () {
+        window.location.href = 'https://www.bbc.com';
+      });
+
+        }
         moveToNextQuestion();
-    } else {
-        alert("Wrong answer, please try again!")
-        answer.value = '';
+        consecutiveWrongAnswers = 0;
         
+
+    } else {
+        consecutiveWrongAnswers++;
+        answer.value = '';  
+    }
+    if (consecutiveWrongAnswers === 1) {
+        alert ("Wrong answer, please try again!");
+    } else if (consecutiveWrongAnswers >= 2) {
+        alert(questions[currentQuestionIndex].hint)  
     }
   });
 
-  question.textContent = questions[currentQuestionIndex].question;
-
-  marker.bindPopup(popUpContent).openPopup();
-
-  
-
-
-howToPlay.addEventListener('click', toggleRules)
-
-
-
-
-function toggleRules() {
+  function toggleRules() {
     if (isShowingRules) {
         const paragraph = howToPlay.querySelector('p');
         if (paragraph) {
@@ -106,6 +107,14 @@ function toggleRules() {
    isShowingRules = true;
     }
 }
+
+  question.textContent = questions[currentQuestionIndex].question;
+
+  marker.bindPopup(popUpContent).openPopup();
+
+  howToPlay.addEventListener('click', toggleRules)
+
+
 
 
 
