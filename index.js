c1513da86a81bc9e8201953bf97d345e522daec0
filomena <filomena.francisco@outlook.com>
2023-08-03 11:19:35 +0100

    still experimenting

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
-const fetch = ('node-fetch-commonjs');
+const fetch = require('node-fetch-commonjs');
 const express = require('express');
 const cors = require('cors');
 const port = process.env.PORT;
