9b8fa9cfef6ea1ea62ddb8f8ddfa378b26f8c85d
DuncanCaplan <duncan.caplan@outlook.com>
2023-08-03 12:28:49 +0100

    local to render

--- frontend/history/index.js
+++ frontend/history/index.js
@@ -50,7 +50,7 @@ document
   .addEventListener("click", getRandomFact);
 
 function getRandomFact() {
-  fetch("http://127.0.0.1:3000/countriesData")
+  fetch("https://lots-of-locations-lol.onrender.com/countriesData")
     .then((response) => response.json())
     .then((data) => {
       console.log(data[0]);
