ca1a0adcde7920caf921e3aebeab8ab652494186
DuncanCaplan <duncan.caplan@outlook.com>
2023-08-03 11:59:09 +0100

    connected history to server

--- frontend/history/index.js
+++ frontend/history/index.js
@@ -1,10 +1,19 @@
-// Get all elements with the class "question-form" and store them in the questionsForms array
-const questionsForms = document.getElementsByClassName("question-form");
-// Get all buttons with the attribute type="button" and store them in the submitButtons array
-const submitButtons = document.querySelectorAll('button[type="button"]');
 const column = document.querySelector(".column");
 
-column.addEventListener("click", () => flipColumn());
+column.addEventListener("click", flipColumn);
+
+// Function to flip the column when clicked
+function flipColumn() {
+  // Check if the column doesn't have the "flipped" class
+  if (!column.classList.contains("flipped")) {
+    // Add the "flipped" class to the column to trigger the flip effect
+    column.classList.add("flipped");
+    // Remove the click event listener from the column after flipping to prevent multiple flips
+    column.removeEventListener("click", () => flipColumn());
+  }
+}
+
+document.getElementById("submitBtn").addEventListener("click", calculateScore);
 
 // Loop through each submit button and add a click event listener that calls the calculateScore() function when clicked
 function calculateScore() {
@@ -31,18 +40,32 @@ function calculateScore() {
     }
   });
 
-  document.getElementById("score").textContent = score;
+  document.getElementById("score").textContent = `${score} out of 4!`;
   document.getElementById("score-box").style.display = "block";
   document.getElementById("submitBtn").disabled = true; // Disable the submit button after the first attempt
 }
 
-// Function to flip the column when clicked
-function flipColumn() {
-  // Check if the column doesn't have the "flipped" class
-  if (!column.classList.contains("flipped")) {
-    // Add the "flipped" class to the column to trigger the flip effect
-    column.classList.add("flipped");
-    // Remove the click event listener from the column after flipping to prevent multiple flips
-    column.removeEventListener("click", () => flipColumn());
-  }
+document
+  .getElementById("randomFactButton")
+  .addEventListener("click", getRandomFact);
+
+function getRandomFact() {
+  fetch("http://127.0.0.1:3000/countriesData")
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data[0]);
+      return data[0];
+    })
+    .then((data) => {
+      console.log(data);
+      const uruguayFacts = data.factsAboutUruguay;
+      const randomFact =
+        uruguayFacts[Math.floor(Math.random() * uruguayFacts.length)];
+
+      document.getElementById("randomFactText").style.display = "block";
+      document.getElementById("randomFactText").innerText = randomFact;
+    })
+    .catch((e) => {
+      console.log("Error fetching random fact", e);
+    });
 }
--- index.js
+++ index.js
@@ -1,64 +1,69 @@
-require("dotenv").configDotenv();
-const express = require('express');
-const cors = require('cors');
-const port = process.env.PORT;
-const apiKey = process.env.API_KEY;
-//const port = 3000;
-const countries = require('./countries.json')
-
+//require("dotenv").configDotenv();
+const express = require("express");
+const cors = require("cors");
+//const port = process.env.PORT;
+//const apiKey = process.env.API_KEY;
+const port = 3000;
+const countries = require("./countries.json");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 // app.use(express.static('frontend'));
-app.use(express.static('frontend/homepage'));
-app.use('/art', express.static('frontend/art'));
-app.use('/music', express.static('frontend/music'));
-app.use('/language', express.static('frontend/language'));
-app.use('/history', express.static('frontend/history'));
-app.use('/countryhomepage', express.static('frontend/countryhomepage'));
-app.use('/map', express.static('frontend/map'));
-app.use('/upload', express.static('frontend/art/gallery'))
-
+app.use(express.static("frontend/homepage"));
+app.use("/art", express.static("frontend/art"));
+app.use("/music", express.static("frontend/music"));
+app.use("/language", express.static("frontend/language"));
+app.use("/history", express.static("frontend/history"));
+app.use("/countryhomepage", express.static("frontend/countryhomepage"));
+app.use("/map", express.static("frontend/map"));
+app.use("/upload", express.static("frontend/art/gallery"));
 
 // app.use(express.static('/frontend/history/history.html'));
 // app.use(express.static('/frontend/music/music.html'));
 // app.use(express.static('/frontend/language/language.html'));
 
-app.get('/', (req, res) => {
-    res.send('frontend/index.html')
-})
+app.get("/", (req, res) => {
+  res.send("frontend/index.html");
+});
 
-app.get('/countriesData', (req, res) => {
-    res.send(countries)
-})
+app.get("/countriesData", (req, res) => {
+  res.send(countries);
+});
 
-app.get('/art', (req, res) => {
-    res.send('/art.index.html')
-})
+app.get("/art", (req, res) => {
+  res.send("/art.index.html");
+});
 
-app.get('/music', (req, res) => {
-    fetch("https://api.spotify.com")
-    .catch((err) => {
-        console.log('rejected', err)
-    })
-    res.send('/music.index.html')
-})
+app.get("/music", (req, res) => {
+  fetch("https://api.spotify.com").catch((err) => {
+    console.log("rejected", err);
+  });
+  res.send("/music.index.html");
+});
 
-app.get('/language', (req, res) => {
-    res.send('/language.index.html')
-})
+app.get("/language", (req, res) => {
+  res.send("/language.index.html");
+});
 
-app.get('/countryhomepage', (req, res) => {
-    res.send('/countryhomepage.index.html')
-})
+app.get(`/history`, (req, res) => {
+  const factsAboutUruguay = countries[0].factsAboutUruguay;
 
-app.get('/map', (req, res) => {
-    res.send('/map.index.html')
-})
+  const randomFact =
+    factsAboutUruguay[Math.floor(Math.random() * factsAboutUruguay.length)];
+
+  res.json({ randomFact });
+});
+
+app.get("/countryhomepage", (req, res) => {
+  res.send("/countryhomepage.index.html");
+});
+
+app.get("/map", (req, res) => {
+  res.send("/map.index.html");
+});
 
 app.listen(port, () => {
-    console.log(`API listening on port ${port}.`);
-})
-
+  console.log(`API listening on port ${port}.`);
+});
